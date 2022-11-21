4edb5a47c26383b5819bb1189287b49ab4aeeb85
Shaan Jassal <shaanjassal101@gmail.com>
2022-11-21 18:15:49 +0000

    Reads and prints out specific num of bytes from disk image
    Signed-off-by: Shaan Jassal <shaanjassal101@gmail.com>

--- Main.c
+++ Main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 void printArray(int arr[], int len){
     for (int i = 0; i < len; i++) {     
@@ -8,18 +12,17 @@ void printArray(int arr[], int len){
     printf("\n");     
 }
 
-int* sectorReader(FILE *fptr, int byteOffset, int bytesToRead){
+int* sectorReader(int fptr, off_t byteOffset, int bytesToRead){
     int* a;
     a = (int*)malloc(bytesToRead * sizeof(int)); //dynamically creating array to hold bytes read
-    fseek(fptr, byteOffset, SEEK_SET); // jumps to byte offset from beginning of file
-    fread(a, sizeof *a, bytesToRead, fptr); // reads into a
+    lseek(fptr, byteOffset, SEEK_SET); // jumps to byte offset from beginning of file
+    read(fptr, a, bytesToRead); // reads into a
     return a;
 }
 
-
 int main(void){
-    FILE *fptr;
-    fptr = fopen("test.txt","r");
+    int fptr;
+    fptr = open("fat16.img",1);
     printArray(sectorReader(fptr, 3, 5), 5); //printing bytes read (a)
-    fclose(fptr);
+    close(fptr);
 }

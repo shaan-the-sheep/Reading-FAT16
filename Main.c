c058ad0bcf47c9efadeebbe4e1d74e4b6ad8b9af
Shaan Jassal <shaanjassal101@gmail.com>
2022-11-23 23:10:13 +0000

    Loaded metadata into boot sector struct
    Signed-off-by: Shaan Jassal <shaanjassal101@gmail.com>

--- Main.c
+++ Main.c
@@ -6,6 +6,29 @@
 #include <unistd.h>
 #include <string.h>
 
+typedef struct __attribute__((__packed__)) {
+uint8_t BS_jmpBoot[ 3 ]; // x86 jump instr. to boot code
+uint8_t BS_OEMName[ 8 ]; // What created the filesystem
+uint16_t BPB_BytsPerSec; // Bytes per Sector
+uint8_t BPB_SecPerClus; // Sectors per Cluster
+uint16_t BPB_RsvdSecCnt; // Reserved Sector Count
+uint8_t BPB_NumFATs; // Number of copies of FAT
+uint16_t BPB_RootEntCnt; // FAT12/FAT16: size of root DIR
+uint16_t BPB_TotSec16; // Sectors, may be 0, see below
+uint8_t BPB_Media; // Media type, e.g. fixed
+uint16_t BPB_FATSz16; // Sectors in FAT (FAT12 or FAT16)
+uint16_t BPB_SecPerTrk; // Sectors per Track
+uint16_t BPB_NumHeads; // Number of heads in disk
+uint32_t BPB_HiddSec; // Hidden Sector count
+uint32_t BPB_TotSec32; // Sectors if BPB_TotSec16 == 0
+uint8_t BS_DrvNum; // 0 = floppy, 0x80 = hard disk
+uint8_t BS_Reserved1; 
+uint8_t BS_BootSig; // Should = 0x29
+uint32_t BS_VolID; // 'Unique' ID for volume
+uint8_t BS_VolLab[ 11 ]; // Non zero terminated string
+uint8_t BS_FilSysType[ 8 ]; // e.g. 'FAT16 ' (Not 0 term.)
+} BootSector;
+
 /**
  * Opens file using open()
  * Prints error message if file could not be opened
@@ -21,17 +44,16 @@ int openFile(char* filename){
     return fp;
 }
 
-void sectorReader(int fp, off_t byteOffset, int bytesToRead){
-    char *buffer = (char *) calloc(500, sizeof(char));
-    lseek(fp, byteOffset, SEEK_CUR); // jumps to byte offset from beginning of file
-    read(fp, buffer, bytesToRead); // reads into buffer
-    printf("The bytes read are [%s]\n",buffer);
-    free(buffer);    
+BootSector sectorReader(int fp, off_t byteOffset, int bytesToRead, BootSector buffer){
+    lseek(fp, byteOffset, SEEK_CUR); // jumps to byte offset 
+    read(fp, &buffer, bytesToRead); // reads into buffer    
+    return buffer;
 }
-
 
 int main(void){
     int fp = openFile("fat16.img");
-    sectorReader(fp, 0, 100000);
+    BootSector bs;
+    bs = sectorReader(fp, 0, sizeof(bs), bs);
+    printf("%x\n",bs.BS_BootSig);
     close(fp);
 }
